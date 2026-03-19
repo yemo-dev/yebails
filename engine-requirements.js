8e06d6cecb8ae065fc01dcd3f14929d155409872
yemobyte <yemobyte@gmail.com>
2026-03-19 04:59:39 -0700

    fix: remove external dependencies from preinstall script to fix yarn install

--- engine-requirements.js
+++ engine-requirements.js
@@ -1,20 +1,12 @@
-const chalk = require("chalk");
-const gradient = require("gradient-string");
-
 const major = parseInt(process.versions.node.split('.')[0], 10);
 
 if (major < 20) {
-  console.log();
-  console.log(
-    chalk.bold(
-      gradient(['#FF0000', '#FFA500'])('   [!] NODE.JS VERSION ERROR [!]   ')
-    )
-  );
+  console.log('\x1b[1m\x1b[31m   [!] NODE.JS VERSION ERROR [!]   \x1b[0m');
   console.log();
   console.error(
-    chalk.red(`   This package requires Node.js 20+ to run reliably.\n`) +
-    chalk.yellow(`   You are using Node.js ${process.versions.node}.\n`) +
-    chalk.cyan(`   Please upgrade to Node.js 20+ to proceed.\n`)
+    `   This package requires Node.js 20+ to run reliably.\n` +
+    `   You are using Node.js ${process.versions.node}.\n` +
+    `   Please upgrade to Node.js 20+ to proceed.\n`
   );
   process.exit(1);
 }
